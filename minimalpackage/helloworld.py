9f466ad52bf8d673759fe28b6c7ad9b4d0ec550d
Monica <monicast@outlook.com>
2019-01-23 14:05:17 -0800

    Added names. Added docstrings

--- minimalpackage/helloworld.py
+++ minimalpackage/helloworld.py
@@ -9,25 +9,29 @@ import os
 from matplotlib import pyplot as plt
 
 def readNameFile():
+    """ Reads the text file of names"""
     current_dir = os.path.dirname(__file__)
     names_file = os.path.join(current_dir, 'hellofile', 'names.txt')
     lines = list(open(names_file))
     return lines
 
 def say_hello(name):
+    """ Say hello to one person """
     print(f'Hello, {name.rstrip()}')
 
 def say_hello_to_everyone():
+    """ Say hello to everyone in the text """
     names = readNameFile()
     for name in names:
         say_hello(name)
 
 def plot_name_length(): 
+    """ Create plot of name lengths """
     names = readNameFile()
     name_lengths = [len(name.rstrip()) for name in names]
     plt.figure()
     plt.ion()
-    plt.plot(name_lengths)
+    plt.plot(name_lengths, marker=11)
     plt.show()
 
 
